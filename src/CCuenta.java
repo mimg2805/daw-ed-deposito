72bc46b16a37e0c70e457183383fa4b6dd613da9
Marcos Miranda <mimg2805@gmail.com>
2026-04-10 21:29:27 +0200

    Añadido código JavaDoc

--- src/CCuenta.java
+++ src/CCuenta.java
@@ -1,15 +1,42 @@
+/**
+ * Una clase que representa una cuenta.
+ */
 public class CCuenta {
 
-
+    /**
+     * El nombre con el que se identifica la cuenta.
+     */
     private String nombre;
+    
+    /**
+     * El número de la cuenta, con guiones.
+     */
     private String cuenta;
+    
+    /**
+     * El saldo actual de la cuenta.
+     */
     private double saldo;
+    
+    /**
+     * El tipo de interés actual de la cuenta.
+     */
     private double tipoInterés;
 
+    /**
+     * Constructor vacío.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Crea una nueva cuenta.
+     * @param nom Nombre de la cuenta.
+     * @param cue Número de la cuenta.
+     * @param sal Saldo inicial de la cuenta.
+     * @param tipo Tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -17,38 +44,67 @@ public class CCuenta {
         saldo=sal;
     }
     
+    /**
+     * Método para obtener el nombre de la cuenta.
+     * @return El nombre actual de la cuenta.
+     */
     public String getNombre() 
     {
         return nombre;
     }
     
+    /**
+     * Método para actualizar el nombre de la cuenta.
+     * @param nombre El nuevo nombre de la cuenta.
+     */
     public void setNombre(String nombre) 
     {
         this.nombre = nombre;
     }
     
+    /**
+     * Método para obtener el número de la cuenta.
+     * @return El número actual de la cuenta.
+     */
     public String getCuenta() 
     {
         return cuenta;
     }
     
+    /**
+     * Método para actualizar el número de la cuenta.
+     * @param cuenta El nuevo número de la cuenta.
+     */
     public void setCuenta(String cuenta) 
     {
         this.cuenta = cuenta;
     }
     
+    /**
+     * Método para obtener el número de la cuenta.
+     * @return El número actual de la cuenta.
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Método para ingresar una cantidad a la cuenta. Modifica el saldo.
+     * @param cantidad La cantidad a ingresar.
+     * @throws Exception No se puede ingresar una cantidad negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    /**
+     * Método para retirar una cantidad de la cuenta. Modifica el saldo.
+     * @param cantidad La cantidad a retirar.
+     * @throws Exception No se puede retirar una cantidad negativa.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -58,11 +114,19 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
     
+    /**
+     * Método para obtener el tipo de interés de la cuenta.
+     * @return El tipo de interés actual de la cuenta.
+     */
     public double getTipoInteres() 
     {
         return tipoInterés;
     }
     
+    /**
+     * Método para actualizar el tipo de interés de la cuenta.
+     * @param tipoInterés El nuevo tipo de interés de la cuenta.
+     */
     public void setTipoInteres(double tipoInterés)
     {
         this.tipoInterés = tipoInterés;
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
+/**
+ * Clase principal.
+ */
 public class Main {
 
+    /**
+     * Método principal.
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es " + saldoActual );
 
         operativa_cuenta(cuenta1, -2300);
         operativa_cuenta(cuenta1, 695);
     }
     
+    /**
+     * Método para realizar operaciones sobre una cuenta. Modifica el saldo.
+     * @param cuenta La cuenta sobre la que se va a operar.
+     * @param cantidad La cantidad. Si es positiva, se hará un ingreso. Si es negativa, se hará un retiro.
+     */
     public static void operativa_cuenta(CCuenta cuenta, float cantidad) {
         if (cantidad < 0) {
             try {
